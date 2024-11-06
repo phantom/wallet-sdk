2894fadeb249a3451848c3526d8fa67ef50bfc74
NAPAS UDOMSAK <14320466+tian000@users.noreply.github.com>
2024-11-05 16:51:04 -0800

    update sdk

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,41 +1,10 @@
-import { DEFAULT_EMBEDDED_ORIGIN } from "./constants";
+import { SDK_URL } from "./constants";
 
-export interface PhantomWalletOptions {
-  embeddedOrigin?: string | RegExp;
-}
-
-const isValidOrigin = (origin: string, embeddedOrigin: string | RegExp) =>
-  typeof embeddedOrigin === "string"
-    ? origin === embeddedOrigin
-    : embeddedOrigin.test(origin);
-
-export function createPhantom(opts: PhantomWalletOptions = {}) {
-  const embeddedOrigin = opts.embeddedOrigin ?? DEFAULT_EMBEDDED_ORIGIN;
-
-  const iframe = document.createElement("iframe");
-
-  iframe.src = "/src/wallet/index.html";
-  iframe.setAttribute("frameborder", "0");
-  iframe.setAttribute("marginwidth", "0");
-  iframe.setAttribute("marginheight", "0");
-  iframe.style.position = "fixed";
-  iframe.style.bottom = "0";
-  iframe.style.right = "0";
-  iframe.style.width = "0";
-  iframe.style.height = "0";
-
-  window.addEventListener("message", (event) => {
-    if (!isValidOrigin(event.origin, embeddedOrigin)) {
-      return;
-    }
-
-    iframe.style.width = `${event.data.width}px`;
-    iframe.style.height = `${event.data.height}px`;
-  });
-
-  document.body.appendChild(iframe);
-
-  return () => {
-    document.body.removeChild(iframe);
-  };
+export function createPhantom() {
+  const container = document.head || document.documentElement;
+  const scriptTag = document.createElement("script");
+  scriptTag.setAttribute("type", "module");
+  scriptTag.setAttribute("src", SDK_URL);
+  container.insertBefore(scriptTag, container.children[0]);
+  container.removeChild(scriptTag);
 }
