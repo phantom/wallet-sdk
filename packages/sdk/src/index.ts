256531d2439fd487554046e6acde2c45c157282d
tian543 <14320466+tian000@users.noreply.github.com>
2024-12-09 20:57:16 +0700

    fix ty-es

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -23,35 +23,35 @@ export function createPhantom (config: CreatePhantomConfig = {}): void {
   const scriptTag = document.createElement('script')
 
   const sdkURL = new URL(SDK_URL)
-  if ('zIndex' in config) {
+  if ('zIndex' in config && config.zIndex != null) {
     sdkURL.searchParams.append('zIndex', config.zIndex.toString())
   }
-  if ('hideLauncherBeforeOnboarded' in config) {
+  if ('hideLauncherBeforeOnboarded' in config && config.hideLauncherBeforeOnboarded != null) {
     sdkURL.searchParams.append(
       'hideLauncherBeforeOnboarded',
       config.hideLauncherBeforeOnboarded.toString()
     )
   }
-  if ('colorScheme' in config) {
+  if ('colorScheme' in config && config.colorScheme != null) {
     sdkURL.searchParams.append('colorScheme', config.colorScheme.toString())
   }
-  if ('paddingBottom' in config) {
+  if ('paddingBottom' in config && config.paddingBottom != null) {
     sdkURL.searchParams.append(
       'paddingBottom',
       config.paddingBottom.toString()
     )
   }
-  if ('paddingRight' in config) {
+  if ('paddingRight' in config && config.paddingRight != null) {
     sdkURL.searchParams.append('paddingRight', config.paddingRight.toString())
   }
 
-  if ('paddingTop || ' in config) {
+  if ('paddingTop' in config && config.paddingTop != null) {
     sdkURL.searchParams.append('paddingTop', config.paddingTop.toString())
   }
-  if ('paddingLeft' in config) {
+  if ('paddingLeft' in config && config.paddingLeft != null) {
     sdkURL.searchParams.append('paddingLeft', config.paddingLeft.toString())
   }
-  if ('position' in config) {
+  if ('position' in config && config.position != null) {
     sdkURL.searchParams.append('position', config.position.toString())
   }
 
