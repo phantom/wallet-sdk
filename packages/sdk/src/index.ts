176a248ba2cd1ae46be1b917a88ea3298b3fc7d4
tian543 <14320466+tian000@users.noreply.github.com>
2024-12-09 20:31:58 +0700

    clean up

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,54 +1,62 @@
-import { SDK_URL } from "./constants.js";
+import { SDK_URL } from './constants.js'
 
 export enum Position {
-  bottomRight = "bottom-right",
-  bottomLeft = "bottom-left",
-  topRight = "top-right",
-  topLeft = "top-left",
+  bottomRight = 'bottom-right',
+  bottomLeft = 'bottom-left',
+  topRight = 'top-right',
+  topLeft = 'top-left',
 }
 
 export type CreatePhantomConfig = Partial<{
-  zIndex: number;
-  hideLauncherBeforeOnboarded: boolean;
-  colorScheme: string;
-  paddingBottom: number;
-  paddingRight: number;
-  paddingTop: number;
-  paddingLeft: number;
-  position: Position;
-}>;
+  zIndex: number
+  hideLauncherBeforeOnboarded: boolean
+  colorScheme: string
+  paddingBottom: number
+  paddingRight: number
+  paddingTop: number
+  paddingLeft: number
+  position: Position
+}>
 
-export function createPhantom(config: CreatePhantomConfig = {}) {
-  const container = document.head || document.documentElement;
-  const scriptTag = document.createElement("script");
+export function createPhantom (config: CreatePhantomConfig = {}): void {
+  const container = document.head ?? document.documentElement
+  const scriptTag = document.createElement('script')
 
-  const sdkURL = new URL(SDK_URL);
-  if (config.zIndex)
-    sdkURL.searchParams.append("zIndex", config.zIndex.toString());
-  if (config.hideLauncherBeforeOnboarded)
+  const sdkURL = new URL(SDK_URL)
+  if ('zIndex' in config) {
+    sdkURL.searchParams.append('zIndex', config.zIndex.toString())
+  }
+  if ('hideLauncherBeforeOnboarded' in config) {
     sdkURL.searchParams.append(
-      "hideLauncherBeforeOnboarded",
-      config.hideLauncherBeforeOnboarded.toString(),
-    );
-  if (config.colorScheme)
-    sdkURL.searchParams.append("colorScheme", config.colorScheme.toString());
-  if (config.paddingBottom)
+      'hideLauncherBeforeOnboarded',
+      config.hideLauncherBeforeOnboarded.toString()
+    )
+  }
+  if ('colorScheme' in config) {
+    sdkURL.searchParams.append('colorScheme', config.colorScheme.toString())
+  }
+  if ('paddingBottom' in config) {
     sdkURL.searchParams.append(
-      "paddingBottom",
-      config.paddingBottom.toString(),
-    );
-  if (config.paddingRight)
-    sdkURL.searchParams.append("paddingRight", config.paddingRight.toString());
+      'paddingBottom',
+      config.paddingBottom.toString()
+    )
+  }
+  if ('paddingRight' in config) {
+    sdkURL.searchParams.append('paddingRight', config.paddingRight.toString())
+  }
 
-  if (config.paddingTop)
-    sdkURL.searchParams.append("paddingTop", config.paddingTop.toString());
-  if (config.paddingLeft)
-    sdkURL.searchParams.append("paddingLeft", config.paddingLeft.toString());
-  if (config.position)
-    sdkURL.searchParams.append("position", config.position.toString());
+  if ('paddingTop || ' in config) {
+    sdkURL.searchParams.append('paddingTop', config.paddingTop.toString())
+  }
+  if ('paddingLeft' in config) {
+    sdkURL.searchParams.append('paddingLeft', config.paddingLeft.toString())
+  }
+  if ('position' in config) {
+    sdkURL.searchParams.append('position', config.position.toString())
+  }
 
-  scriptTag.setAttribute("type", "module");
-  scriptTag.setAttribute("src", sdkURL.toString());
-  container.insertBefore(scriptTag, container.children[0]);
-  container.removeChild(scriptTag);
+  scriptTag.setAttribute('type', 'module')
+  scriptTag.setAttribute('src', sdkURL.toString())
+  container.insertBefore(scriptTag, container.children[0])
+  container.removeChild(scriptTag)
 }
