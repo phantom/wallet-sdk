cf5f97ec50bff8fe0d33edaf90ef98a22fa021d2
Jakub Adamczyk <jakub.adamczyk@phantom.app>
2025-05-22 21:04:43 +0200

    port over to react sdk

--- packages/react-sdk/src/solana/useProvider.ts
+++ packages/react-sdk/src/solana/useProvider.ts
@@ -1,5 +1,15 @@
-import { getProvider } from "@phantom/browser-sdk/solana";
+import { usePhantom } from "../PhantomContext";
 
 export function useProvider() {
-  return getProvider();
+  const { phantom } = usePhantom();
+
+  if (!phantom) {
+    throw new Error("Phantom is not available, check if you configured the PhantomProvider properly.");
+  }
+
+  if (!phantom?.solana) {
+    throw new Error("Solana is not available, check if you configured the solana plugin properly.");
+  }
+
+  return phantom.solana.getProvider();
 }
