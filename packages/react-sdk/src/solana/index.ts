f8bfde862752524c253f25f4f52dda485a9dc6d9
Jakub Adamczyk <jakub.adamczyk@phantom.app>
2025-05-30 14:22:06 +0200

    add react sdk demo

--- packages/react-sdk/src/solana/index.ts
+++ packages/react-sdk/src/solana/index.ts
@@ -2,3 +2,4 @@ import "@phantom/browser-sdk/solana";
 export { useProvider } from "./useProvider";
 export { useConnect } from "./useConnect";
 export { useDisconnect } from "./useDisconnect";
+export { useAccount } from "./useAccount";
