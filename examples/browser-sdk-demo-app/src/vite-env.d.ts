18740c00a7c6cf47d7ed68fb4db35e107456355d
rafael pedrola <rafael@phantom.app>
2025-09-03 21:05:24 +0200

    fix lint

---
+++ examples/browser-sdk-demo-app/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_ID: string
+  readonly VITE_API_BASE_URL: string
+  readonly VITE_AUTH_URL: string
+  readonly VITE_REDIRECT_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
