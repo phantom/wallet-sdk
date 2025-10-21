6c057db54ba75e9fab43d30dac8ce7559ff560ce
rafael pedrola <rafael@phantom.app>
2025-10-21 12:06:57 +0200

    disable authenticator renewal

--- packages/browser-sdk/src/utils/auth-callback.test.ts
+++ packages/browser-sdk/src/utils/auth-callback.test.ts
@@ -1,4 +1,3 @@
-import { describe, it, expect, beforeEach, afterEach } from '@jest/globals';
 import { isAuthFailureCallback, isAuthCallbackUrl } from './auth-callback';
 
 describe('auth-callback utilities', () => {
